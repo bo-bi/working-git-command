32a0b3e909c6a14c3e3e1615cd5fbda2b22637f1
WH <18935445147@qq.com>
2019-02-19 12:36:23 +0800

    First commit.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  base: '/working-git-command/',
+  title: 'Git Command',
+  description: '在工作中，经常用到的git命令.',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+  ]
+}
---
+++ docs/README.md
@@ -0,0 +1,3 @@
+echo '# Hello VuePress!'
+<h2> 123 </h2>
+<img :src="$withBase('/favicon.ico')" alt="foo">
